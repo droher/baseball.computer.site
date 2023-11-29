24c3e57edf537483333c8f682d244d0953bf3f95
David Roher <david.roher@gmail.com>
2023-11-29 16:41:26 -0500

    Add font conf

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,11 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        special: ["Special Elite"]
+      }
+    }
   },
 
   plugins: [forms, typography, daisyui],
