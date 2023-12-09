3dce8eb071012d89180b9bf21a15564abcf49260
David Roher <david.roher@gmail.com>
2023-12-09 15:05:52 -0500

    Add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
   import "../global.css";
   import Navbar from "$lib/components/app/Nav.svelte";
   import Footer from "$lib/components/app/Footer.svelte";
+
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+ 
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="flex flex-col min-h-screen">
